b32748c691357a4a55f7351bfc1b441c63842526
vijayvykuntam <vijay.vykuntam@hmcts.net>
2020-03-26 14:31:21 +0000

    updated the code for adding manage cases

--- src/gatling/simulations/uk/gov/hmcts/reform/exui/performance/scenarios/utils/Environment.scala
+++ src/gatling/simulations/uk/gov/hmcts/reform/exui/performance/scenarios/utils/Environment.scala
@@ -4,15 +4,13 @@ import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
 object Environment {
-
+ val manageOrdURL="https://xui-mo-webapp-perftest.service.core-compute-perftest.internal"
   val idamURL = "https://idam-web-public.perftest.platform.hmcts.net"
   val baseURL = "https://xui-webapp-perftest.service.core-compute-perftest.internal"
-
-
-  /*val idamURL = "https://idam-web-public.aat.platform.hmcts.net"
+ // val idamURL = "https://idam-web-public.aat.platform.hmcts.net"
   val ccdEnvurl = "https://ccd-case-management-web-aat.service.core-compute-aat.internal"
-  val baseURL = "https://ccd-api-gateway-web-aat.service.core-compute-aat.internal"*/
-
+  //val baseURL = "https://xui-webapp-aat.service.core-compute-aat.internal"
+ val baseFPLAURL = "https://ccd-case-management-web-aat.service.core-compute-aat.internal"
   // val baseURL = "https://gateway.ccd.demo.platform.hmcts.net"
   //val idamURL = "https://idam.preprod.ccidam.reform.hmcts.net"
   //val ccdEnvurl = "https://www.ccd.demo.platform.hmcts.net"
