8d962871b448b26d22a9e77fcba055dbc373fc64
vijayvykuntam <vijay.vykuntam@hmcts.net>
2020-04-30 16:07:22 +0100

    updated the code for adding manage cases
    (cherry picked from commit 5164fe5)

--- src/gatling/simulations/uk/gov/hmcts/reform/exui/performance/scenarios/EXUIIACMC.scala
+++ src/gatling/simulations/uk/gov/hmcts/reform/exui/performance/scenarios/EXUIIACMC.scala
@@ -16,12 +16,24 @@ object EXUIIACMC {
   //val BaseURL = Environment.baseURL
   val IdamUrl = Environment.idamURL
   val baseURL=Environment.baseURL
-  val loginFeeder = csv("OrgId.csv").circular
+  //val loginFeeder = csv("OrgId.csv").circular
+  val feedUserDataIAC = csv("IACUserData.csv").circular
 
   val MinThinkTime = Environment.minThinkTime
   val MaxThinkTime = Environment.maxThinkTime
 
   //headers
+
+  val headers_tc_appeal = Map(
+    "accept" -> "application/json",
+    "accept-encoding" -> "gzip, deflate, br",
+    "accept-language" -> "en-US,en;q=0.9",
+    "content-type" -> "application/json",
+    "sec-fetch-dest" -> "empty",
+    "sec-fetch-mode" -> "cors",
+    "sec-fetch-site" -> "same-origin",
+    "x-dtpc" -> "3$38734236_77h19vDTRMSASFKPLKDRFKMHCCHMMCARPGMHGD-0",
+    "x-dtreferer" -> "https://manage-case.perftest.platform.hmcts.net/accept-terms-and-conditions")
 
   val headers_tc = Map(
     "Content-Type" -> "application/json",
@@ -284,52 +296,106 @@ object EXUIIACMC {
     "Sec-Fetch-Site" -> "same-origin",
     "experimental" -> "true")
 
-  val manageCasesHomePage =
-    feed(loginFeeder)
-    .exec(http("XUI-IAC01_010_Homepage")
+  val headers_hometc = Map(
+    "accept" -> "application/json, text/plain, */*",
+    "accept-encoding" -> "gzip, deflate, br",
+    "accept-language" -> "en-US,en;q=0.9",
+    "sec-fetch-dest" -> "empty",
+    "sec-fetch-mode" -> "cors",
+    "sec-fetch-site" -> "same-origin",
+    "x-dtpc" -> "3$38717637_792h4vDTRMSASFKPLKDRFKMHCCHMMCARPGMHGD-0")
+
+  val headers_38 = Map(
+    "accept" -> "application/json, text/plain, */*",
+    "accept-encoding" -> "gzip, deflate, br",
+    "accept-language" -> "en-US,en;q=0.9",
+    "sec-fetch-dest" -> "empty",
+    "sec-fetch-mode" -> "cors",
+    "sec-fetch-site" -> "same-origin",
+    "x-dtpc" -> "3$38732415_350h4vDTRMSASFKPLKDRFKMHCCHMMCARPGMHGD-0")
+
+  val headers_42 = Map(
+    "accept" -> "application/json, text/plain, */*",
+    "accept-encoding" -> "gzip, deflate, br",
+    "accept-language" -> "en-US,en;q=0.9",
+    "sec-fetch-dest" -> "empty",
+    "sec-fetch-mode" -> "cors",
+    "sec-fetch-site" -> "same-origin",
+    "x-dtpc" -> "3$38732415_350h7vDTRMSASFKPLKDRFKMHCCHMMCARPGMHGD-0")
+
+  val headers_access_read = Map(
+    "accept" -> "application/json",
+    "accept-encoding" -> "gzip, deflate, br",
+    "accept-language" -> "en-US,en;q=0.9",
+    "content-type" -> "application/json",
+    "sec-fetch-dest" -> "empty",
+    "sec-fetch-mode" -> "cors",
+    "sec-fetch-site" -> "same-origin",
+    "x-dtpc" -> "3$38734236_77h15vDTRMSASFKPLKDRFKMHCCHMMCARPGMHGD-0",
+    "x-dtreferer" -> "https://manage-case.perftest.platform.hmcts.net/accept-terms-and-conditions")
+
+  val manageCasesHomePage =group("XUI01_010_Homepage") {
+    // feed(loginFeeder)
+    exec(http("XUI01_010_005_Homepage")
       .get("/")
       .headers(headers_0)
       .check(status.is(200)))
+      .pause(MinThinkTime seconds, MaxThinkTime seconds)
 
-    .exec(http("XUI-IAC01_020_LoginLandingPage")
+      .exec(http("XUI01_010_010_HomepageTCEnabled")
+      .get("/api/configuration?configurationKey=termsAndConditionsEnabled")
+      .headers(headers_hometc)
+      .check(status.is(200)))
+
+      .exec(http("XUI01_010_015_HompageLoginPage")
       .get(IdamUrl + "/login?response_type=code&client_id=xuiwebapp&redirect_uri=" + baseURL + "/oauth2/callback&scope=profile%20openid%20roles%20manage-user%20create-user")
       .headers(headers_login)
       .check(regex("Sign in"))
-      .check(css("input[name='_csrf']", "value").saveAs("csrfToken")))
+      .check(css("input[name='_csrf']", "value").saveAs("csrfToken"))
+      .check(status.is(200)))
+      .pause(MinThinkTime seconds, MaxThinkTime seconds)
+  }
 
-    .pause(MinThinkTime seconds, MaxThinkTime seconds)
-
-  val feedUserDataIAC = csv("IACUserData.csv").circular
-
-  val manageCaseslogin =
+  val manageCaseslogin =group("XUI01_020_SignIn") {
 
     feed(feedUserDataIAC)
 
-    .exec(http("XUI-IAC01_030_SubmitLoginPage")
-      .post(IdamUrl + "/login?response_type=code&client_id=xuiwebapp&redirect_uri=" + baseURL + "/oauth2/callback&scope=profile%20openid%20roles%20manage-user%20create-user")
-      .formParam("username", "${IACUserName}")
-      .formParam("password", "Pass19word")
-      .formParam("save", "Sign in")
-      .formParam("selfRegistrationEnabled", "false")
-      .formParam("_csrf", "${csrfToken}")
-      .headers(headers_login_submit)
-      .check(status.in(200, 304, 302))
-      //.check(currentLocation.saveAs("currentPage"))
-     // .check(regex("Terms and conditions").count.saveAs("contentstatus"))
-     // .check(headerRegex("Set-Cookie", "__userid__=(.*)”).saveAs("userid")))
-    // .check(headerRegex("StoredCookie", "__userid__=(.*)").saveAs("authCookie"))
-          )
-          .pause(MinThinkTime seconds, MaxThinkTime seconds)
+      .exec(http("XUI01_020_005_SignIn")
+        .post(IdamUrl + "/login?response_type=code&client_id=xuiwebapp&redirect_uri=" + baseURL + "/oauth2/callback&scope=profile%20openid%20roles%20manage-user%20create-user")
+        .formParam("username", "${IACUserName}")
+        .formParam("password", "Pass19word")
+        .formParam("save", "Sign in")
+        .formParam("selfRegistrationEnabled", "false")
+        .formParam("_csrf", "${csrfToken}")
+        .headers(headers_login_submit)
+        .check(status.in(200, 304, 302))
+      )
+      .pause(MinThinkTime seconds, MaxThinkTime seconds)
 
-
-    .exec(getCookieValue(
-      CookieKey("__userid__").withDomain("manage-case.perftest.platform.hmcts.net").saveAs("myUserId")))
-  .exec {
+      .exec(getCookieValue(
+        CookieKey("__userid__").withDomain("manage-case.perftest.platform.hmcts.net").saveAs("myUserId")))
+      /*.exec {
          session =>
            println("current page is ....." + session("myUserId").as[String])
 
            session
-       }
+       }*/
+
+      .repeat(2,"n") {
+      exec(http("XUI01_020_010_SignInTCEnabled${n}")
+        .get("/api/configuration?configurationKey=termsAndConditionsEnabled")
+        .headers(headers_38)
+        .check(status.in(200, 304)))
+      /*.get("/api/configuration?configurationKey=termsAndConditionsEnabled")
+        .headers(headers_42)*/
+    }
+      .exec(http("XUI01_020_015_SignInAcceptTCGet")
+        .get("/accept-terms-and-conditions")
+        .headers(headers_tc_get)
+        .check(status.in(200, 304))
+      )
+
+      }
 
   val termsandconditions_Get=
     exec(http("tc_get")
@@ -337,30 +403,46 @@ object EXUIIACMC {
       .headers(headers_tc_get)
       .check(status.in(200, 304)))
 
-  val termsnconditions=
+
+  val termsnconditions=group("XUI01_030_AcceptT&C") {
     //doIf(session => session.contains("accessToken")) {
-      exec(http("XUI-IAC01_040_T&C")
+      exec(http("XUI01_030_005_AcceptT&C")
         .post("/api/userTermsAndConditions")
         .headers(headers_tc)
-        //.body(ElFileBody("RecordedSimulationTC_0031_request.json")).asJson
         .body(StringBody("{\"userId\":\"${myUserId}\"}"))
-
         .check(status.in(200, 304, 302))
       )
-   // }
       .pause(MinThinkTime seconds, MaxThinkTime seconds)
 
+      .exec(http("XUI01_030_010_AcceptT&CEnabled")
+    .get("/api/configuration?configurationKey=termsAndConditionsEnabled")
+    .headers(headers_hometc)
+      .check(status.in(200,304,302))
+      )
+.repeat(4,"count") {
+  exec(http("XUI01_030_015_AcceptT&CAccessJurisdictions${count}")
+        .get("/aggregated/caseworkers/:uid/jurisdictions?access=read")
+      .headers(headers_access_read)
+    .check(status.in(200,304,302)))
 
+     }
+.exec(http("XUI01_030_020_AcceptT&C_TCEnabledAppealStartedPage")
+    .get("/data/caseworkers/:uid/jurisdictions/IA/case-types/Asylum/cases/pagination_metadata?state=appealStarted")
+    .headers(headers_tc_appeal)
+    .check(status.in(200,304,302)))
+          .exec(http("XUI01_030_025AcceptT&CEnabledAppealStartedPage1")
+    .get("/aggregated/caseworkers/:uid/jurisdictions/IA/case-types/Asylum/cases?view=WORKBASKET&state=appealStarted&page=1")
+    .headers(headers_tc_appeal)
+    .check(status.in(200,304,302)))
+  }
 
-
-
-  val manageCase_Logout =
-    exec(http("XUI-IAC01_050__Logout")
+  val manageCase_Logout =group("XUI01_040_SignOut") {
+    exec(http("XUI01_040_005_SignOut")
       .get("/api/logout")
       .headers(headers_34)
       //.check(regex("Sign in")))
-      .check(status.in(200,304,302)))
-
+      .check(status.in(200, 304, 302)))
+  }
 
   private val rng: Random = new Random()
   private def firstName(): String = rng.alphanumeric.take(10).mkString
