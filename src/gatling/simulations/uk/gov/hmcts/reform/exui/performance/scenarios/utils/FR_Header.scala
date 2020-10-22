38b489cc606cca46ef02618a27e0ac717c6f1a6c
Amogh Naik <amoghvnaik@amoghs-mbp.home>
2020-10-22 17:08:50 +0100

    added FR

---
+++ src/gatling/simulations/uk/gov/hmcts/reform/exui/performance/scenarios/utils/FR_Header.scala
@@ -0,0 +1,418 @@
+package uk.gov.hmcts.reform.exui.performance.scenarios.utils
+
+object FR_Header {
+
+  val headers_0 = Map(
+    "Accept" -> "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "Accept-Encoding" -> "gzip, deflate, br",
+    "Accept-Language" -> "en-GB,en-US;q=0.9,en;q=0.8",
+    "Origin" -> "https://idam-web-public.demo.platform.hmcts.net",
+    "Sec-Fetch-Dest" -> "document",
+    "Sec-Fetch-Mode" -> "navigate",
+    "Sec-Fetch-Site" -> "same-origin",
+    "Sec-Fetch-User" -> "?1",
+    "Upgrade-Insecure-Requests" -> "1",
+    "sec-ch-ua" -> """Chromium";v="86", "\"Not\\A;Brand";v="99", "Google Chrome";v="86""",
+    "sec-ch-ua-mobile" -> "?0")
+
+  val headers_5 = Map(
+    "Accept" -> "*/*",
+    "Accept-Encoding" -> "gzip, deflate, br",
+    "Accept-Language" -> "en-GB,en-US;q=0.9,en;q=0.8",
+    "Sec-Fetch-Dest" -> "empty",
+    "Sec-Fetch-Mode" -> "cors",
+    "Sec-Fetch-Site" -> "same-origin",
+    "sec-ch-ua" -> """Chromium";v="86", "\"Not\\A;Brand";v="99", "Google Chrome";v="86""",
+    "sec-ch-ua-mobile" -> "?0")
+
+  val headers_6 = Map(
+    "Accept" -> "application/json, text/plain, */*",
+    "Accept-Encoding" -> "gzip, deflate, br",
+    "Accept-Language" -> "en-GB,en-US;q=0.9,en;q=0.8",
+    "Sec-Fetch-Dest" -> "empty",
+    "Sec-Fetch-Mode" -> "cors",
+    "Sec-Fetch-Site" -> "same-origin",
+    "sec-ch-ua" -> """Chromium";v="86", "\"Not\\A;Brand";v="99", "Google Chrome";v="86""",
+    "sec-ch-ua-mobile" -> "?0")
+
+  val headers_20 = Map(
+    "Accept" -> "application/json",
+    "Accept-Encoding" -> "gzip, deflate, br",
+    "Accept-Language" -> "en-GB,en-US;q=0.9,en;q=0.8",
+    "Content-Type" -> "application/json",
+    "Sec-Fetch-Dest" -> "empty",
+    "Sec-Fetch-Mode" -> "cors",
+    "Sec-Fetch-Site" -> "same-origin",
+    "X-XSRF-TOKEN" -> "3QG3De0Z--dioSHujIGaJtPl3Z2Lwo9zE8EA",
+    "sec-ch-ua" -> """Chromium";v="86", "\"Not\\A;Brand";v="99", "Google Chrome";v="86""",
+    "sec-ch-ua-mobile" -> "?0")
+
+  val headers_29 = Map(
+    "Accept" -> "application/json",
+    "Accept-Encoding" -> "gzip, deflate, br",
+    "Accept-Language" -> "en-GB,en-US;q=0.9,en;q=0.8",
+    "Content-Type" -> "application/json",
+    "Request-Id" -> "|A3zwi.s6j/r",
+    "Sec-Fetch-Dest" -> "empty",
+    "Sec-Fetch-Mode" -> "cors",
+    "Sec-Fetch-Site" -> "same-origin",
+    "X-XSRF-TOKEN" -> "kdWIhL0b-SMOmRR9DhvHLAwGtGSuSSkdu12Q",
+    "sec-ch-ua" -> """Chromium";v="86", "\"Not\\A;Brand";v="99", "Google Chrome";v="86""",
+    "sec-ch-ua-mobile" -> "?0")
+
+  val headers_35 = Map(
+    "Accept" -> "application/vnd.uk.gov.hmcts.ccd-data-store-api.ui-start-case-trigger.v2+json;charset=UTF-8",
+    "Accept-Encoding" -> "gzip, deflate, br",
+    "Accept-Language" -> "en-GB,en-US;q=0.9,en;q=0.8",
+    "Content-Type" -> "application/json",
+    "Request-Id" -> "|A3zwi.m06QO",
+    "Sec-Fetch-Dest" -> "empty",
+    "Sec-Fetch-Mode" -> "cors",
+    "Sec-Fetch-Site" -> "same-origin",
+    "X-XSRF-TOKEN" -> "VL8z5gBK-1CbfeGu8dAvEquxm1gq3ZY2p85c",
+    "experimental" -> "true",
+    "sec-ch-ua" -> """Chromium";v="86", "\"Not\\A;Brand";v="99", "Google Chrome";v="86""",
+    "sec-ch-ua-mobile" -> "?0")
+
+  val headers_38 = Map(
+    "Accept" -> "application/vnd.uk.gov.hmcts.ccd-data-store-api.ui-start-case-trigger.v2+json;charset=UTF-8",
+    "Accept-Encoding" -> "gzip, deflate, br",
+    "Accept-Language" -> "en-GB,en-US;q=0.9,en;q=0.8",
+    "Content-Type" -> "application/json",
+    "Request-Id" -> "|A3zwi.lQFp/",
+    "Sec-Fetch-Dest" -> "empty",
+    "Sec-Fetch-Mode" -> "cors",
+    "Sec-Fetch-Site" -> "same-origin",
+    "X-XSRF-TOKEN" -> "XzWB2w7u-Tum87zVow1pHbKEaIB95SoADXsg",
+    "experimental" -> "true",
+    "sec-ch-ua" -> """Chromium";v="86", "\"Not\\A;Brand";v="99", "Google Chrome";v="86""",
+    "sec-ch-ua-mobile" -> "?0")
+
+  val headers_40 = Map(
+    "Accept" -> "application/vnd.uk.gov.hmcts.ccd-data-store-api.ui-user-profile.v2+json;charset=UTF-8",
+    "Accept-Encoding" -> "gzip, deflate, br",
+    "Accept-Language" -> "en-GB,en-US;q=0.9,en;q=0.8",
+    "Content-Type" -> "application/json",
+    "Request-Context" -> "appId=cid-v1:97eb6d07-2397-40dd-a595-917d825a8fdc",
+    "Request-Id" -> "|A3zwi.GrYm/",
+    "Sec-Fetch-Dest" -> "empty",
+    "Sec-Fetch-Mode" -> "cors",
+    "Sec-Fetch-Site" -> "same-origin",
+    "X-XSRF-TOKEN" -> "mUKWrAha-59l2qlaCMuAHFSVj1e6HMnMvT4A",
+    "experimental" -> "true",
+    "sec-ch-ua" -> """Chromium";v="86", "\"Not\\A;Brand";v="99", "Google Chrome";v="86""",
+    "sec-ch-ua-mobile" -> "?0")
+
+  val headers_41 = Map(
+    "Accept" -> "application/vnd.uk.gov.hmcts.ccd-data-store-api.case-data-validate.v2+json;charset=UTF-8",
+    "Accept-Encoding" -> "gzip, deflate, br",
+    "Accept-Language" -> "en-GB,en-US;q=0.9,en;q=0.8",
+    "Content-Type" -> "application/json",
+    "Origin" -> "https://manage-case.demo.platform.hmcts.net",
+    "Request-Context" -> "appId=cid-v1:97eb6d07-2397-40dd-a595-917d825a8fdc",
+    "Request-Id" -> "|A3zwi.RwZlI",
+    "Sec-Fetch-Dest" -> "empty",
+    "Sec-Fetch-Mode" -> "cors",
+    "Sec-Fetch-Site" -> "same-origin",
+    "X-XSRF-TOKEN" -> "dbkR5T6x-i2uo75il1hrXnFEmm2wLhhJ0IxA",
+    "experimental" -> "true",
+    "sec-ch-ua" -> """Chromium";v="86", "\"Not\\A;Brand";v="99", "Google Chrome";v="86""",
+    "sec-ch-ua-mobile" -> "?0")
+
+  val headers_43 = Map(
+    "Accept" -> "application/json, text/plain, */*",
+    "Accept-Encoding" -> "gzip, deflate, br",
+    "Accept-Language" -> "en-GB,en-US;q=0.9,en;q=0.8",
+    "Request-Context" -> "appId=cid-v1:97eb6d07-2397-40dd-a595-917d825a8fdc",
+    "Request-Id" -> "|A3zwi.4iXXr",
+    "Sec-Fetch-Dest" -> "empty",
+    "Sec-Fetch-Mode" -> "cors",
+    "Sec-Fetch-Site" -> "same-origin",
+    "sec-ch-ua" -> """Chromium";v="86", "\"Not\\A;Brand";v="99", "Google Chrome";v="86""",
+    "sec-ch-ua-mobile" -> "?0")
+
+  val headers_45 = Map(
+    "Accept" -> "application/json",
+    "Accept-Encoding" -> "gzip, deflate, br",
+    "Accept-Language" -> "en-GB,en-US;q=0.9,en;q=0.8",
+    "Content-Type" -> "application/json",
+    "Request-Context" -> "appId=cid-v1:97eb6d07-2397-40dd-a595-917d825a8fdc",
+    "Request-Id" -> "|A3zwi.PyfSk",
+    "Sec-Fetch-Dest" -> "empty",
+    "Sec-Fetch-Mode" -> "cors",
+    "Sec-Fetch-Site" -> "same-origin",
+    "X-XSRF-TOKEN" -> "0iT7RzKC-nLrWmOck_puwVmSMLcVsNJSXcMw",
+    "sec-ch-ua" -> """Chromium";v="86", "\"Not\\A;Brand";v="99", "Google Chrome";v="86""",
+    "sec-ch-ua-mobile" -> "?0")
+
+  val headers_48 = Map(
+    "Accept" -> "application/vnd.uk.gov.hmcts.ccd-data-store-api.case-data-validate.v2+json;charset=UTF-8",
+    "Accept-Encoding" -> "gzip, deflate, br",
+    "Accept-Language" -> "en-GB,en-US;q=0.9,en;q=0.8",
+    "Content-Type" -> "application/json",
+    "Origin" -> "https://manage-case.demo.platform.hmcts.net",
+    "Request-Context" -> "appId=cid-v1:97eb6d07-2397-40dd-a595-917d825a8fdc",
+    "Request-Id" -> "|A3zwi.60AeS",
+    "Sec-Fetch-Dest" -> "empty",
+    "Sec-Fetch-Mode" -> "cors",
+    "Sec-Fetch-Site" -> "same-origin",
+    "X-XSRF-TOKEN" -> "UPnNsmwa--Hrsa15keg7kuVmkZBz2NvwDGCQ",
+    "experimental" -> "true",
+    "sec-ch-ua" -> """Chromium";v="86", "\"Not\\A;Brand";v="99", "Google Chrome";v="86""",
+    "sec-ch-ua-mobile" -> "?0")
+
+  val headers_52 = Map(
+    "Accept" -> "application/vnd.uk.gov.hmcts.ccd-data-store-api.case-data-validate.v2+json;charset=UTF-8",
+    "Accept-Encoding" -> "gzip, deflate, br",
+    "Accept-Language" -> "en-GB,en-US;q=0.9,en;q=0.8",
+    "Content-Type" -> "application/json",
+    "Origin" -> "https://manage-case.demo.platform.hmcts.net",
+    "Request-Context" -> "appId=cid-v1:97eb6d07-2397-40dd-a595-917d825a8fdc",
+    "Request-Id" -> "|A3zwi.3MsbI",
+    "Sec-Fetch-Dest" -> "empty",
+    "Sec-Fetch-Mode" -> "cors",
+    "Sec-Fetch-Site" -> "same-origin",
+    "X-XSRF-TOKEN" -> "YI4UlQYl-p0x-yDKccgEYBkead0QTFEzFpKk",
+    "experimental" -> "true",
+    "sec-ch-ua" -> """Chromium";v="86", "\"Not\\A;Brand";v="99", "Google Chrome";v="86""",
+    "sec-ch-ua-mobile" -> "?0")
+
+  val headers_54 = Map(
+    "Accept" -> "application/vnd.uk.gov.hmcts.ccd-data-store-api.case-data-validate.v2+json;charset=UTF-8",
+    "Accept-Encoding" -> "gzip, deflate, br",
+    "Accept-Language" -> "en-GB,en-US;q=0.9,en;q=0.8",
+    "Content-Type" -> "application/json",
+    "Origin" -> "https://manage-case.demo.platform.hmcts.net",
+    "Request-Context" -> "appId=cid-v1:97eb6d07-2397-40dd-a595-917d825a8fdc",
+    "Request-Id" -> "|A3zwi.W1xA5",
+    "Sec-Fetch-Dest" -> "empty",
+    "Sec-Fetch-Mode" -> "cors",
+    "Sec-Fetch-Site" -> "same-origin",
+    "X-XSRF-TOKEN" -> "wHOyjXhE-WuqJf_7xDys_7bNVYLEazsuWHn8",
+    "experimental" -> "true",
+    "sec-ch-ua" -> """Chromium";v="86", "\"Not\\A;Brand";v="99", "Google Chrome";v="86""",
+    "sec-ch-ua-mobile" -> "?0")
+
+  val headers_56 = Map(
+    "Accept" -> "application/json, text/plain, */*",
+    "Accept-Encoding" -> "gzip, deflate, br",
+    "Accept-Language" -> "en-GB,en-US;q=0.9,en;q=0.8",
+    "Request-Context" -> "appId=cid-v1:97eb6d07-2397-40dd-a595-917d825a8fdc",
+    "Request-Id" -> "|A3zwi.49L79",
+    "Sec-Fetch-Dest" -> "empty",
+    "Sec-Fetch-Mode" -> "cors",
+    "Sec-Fetch-Site" -> "same-origin",
+    "sec-ch-ua" -> """Chromium";v="86", "\"Not\\A;Brand";v="99", "Google Chrome";v="86""",
+    "sec-ch-ua-mobile" -> "?0")
+
+  val headers_62 = Map(
+    "Accept" -> "application/json",
+    "Accept-Encoding" -> "gzip, deflate, br",
+    "Accept-Language" -> "en-GB,en-US;q=0.9,en;q=0.8",
+    "Content-Type" -> "application/json",
+    "Request-Context" -> "appId=cid-v1:97eb6d07-2397-40dd-a595-917d825a8fdc",
+    "Request-Id" -> "|A3zwi.4Xu74",
+    "Sec-Fetch-Dest" -> "empty",
+    "Sec-Fetch-Mode" -> "cors",
+    "Sec-Fetch-Site" -> "same-origin",
+    "X-XSRF-TOKEN" -> "ML9xHhr2-MfIams5KYwCtT5xRV8lYwoDXgMk",
+    "sec-ch-ua" -> """Chromium";v="86", "\"Not\\A;Brand";v="99", "Google Chrome";v="86""",
+    "sec-ch-ua-mobile" -> "?0")
+
+  val headers_64 = Map(
+    "Accept" -> "application/vnd.uk.gov.hmcts.ccd-data-store-api.case-data-validate.v2+json;charset=UTF-8",
+    "Accept-Encoding" -> "gzip, deflate, br",
+    "Accept-Language" -> "en-GB,en-US;q=0.9,en;q=0.8",
+    "Content-Type" -> "application/json",
+    "Origin" -> "https://manage-case.demo.platform.hmcts.net",
+    "Request-Context" -> "appId=cid-v1:97eb6d07-2397-40dd-a595-917d825a8fdc",
+    "Request-Id" -> "|A3zwi.7YXrY",
+    "Sec-Fetch-Dest" -> "empty",
+    "Sec-Fetch-Mode" -> "cors",
+    "Sec-Fetch-Site" -> "same-origin",
+    "X-XSRF-TOKEN" -> "cT1J236s-tqzAZLYsQiUwTCNwDHfxAwuE0WU",
+    "experimental" -> "true",
+    "sec-ch-ua" -> """Chromium";v="86", "\"Not\\A;Brand";v="99", "Google Chrome";v="86""",
+    "sec-ch-ua-mobile" -> "?0")
+
+  val headers_65 = Map(
+    "Accept" -> "application/vnd.uk.gov.hmcts.ccd-data-store-api.case-data-validate.v2+json;charset=UTF-8",
+    "Accept-Encoding" -> "gzip, deflate, br",
+    "Accept-Language" -> "en-GB,en-US;q=0.9,en;q=0.8",
+    "Content-Type" -> "application/json",
+    "Origin" -> "https://manage-case.demo.platform.hmcts.net",
+    "Request-Context" -> "appId=cid-v1:97eb6d07-2397-40dd-a595-917d825a8fdc",
+    "Request-Id" -> "|A3zwi.I26wm",
+    "Sec-Fetch-Dest" -> "empty",
+    "Sec-Fetch-Mode" -> "cors",
+    "Sec-Fetch-Site" -> "same-origin",
+    "X-XSRF-TOKEN" -> "Tp1Fl7zt-MEQt-HC60_SFJ8n7wze71HASP5w",
+    "experimental" -> "true",
+    "sec-ch-ua" -> """Chromium";v="86", "\"Not\\A;Brand";v="99", "Google Chrome";v="86""",
+    "sec-ch-ua-mobile" -> "?0")
+
+  val headers_72 = Map(
+    "Accept" -> "",
+    "Accept-Encoding" -> "gzip, deflate, br",
+    "Accept-Language" -> "en-GB,en-US;q=0.9,en;q=0.8",
+    "Content-Type" -> "multipart/form-data; boundary=----WebKitFormBoundaryOk0cBe5utcv98foi",
+    "Origin" -> "https://manage-case.demo.platform.hmcts.net",
+    "Request-Context" -> "appId=cid-v1:97eb6d07-2397-40dd-a595-917d825a8fdc",
+    "Request-Id" -> "|A3zwi.52oVv",
+    "Sec-Fetch-Dest" -> "empty",
+    "Sec-Fetch-Mode" -> "cors",
+    "Sec-Fetch-Site" -> "same-origin",
+    "X-XSRF-TOKEN" -> "Z5HN1jNi-w9hf_-68M98eX6ECHrqsv7TjvaU",
+    "sec-ch-ua" -> """Chromium";v="86", "\"Not\\A;Brand";v="99", "Google Chrome";v="86""",
+    "sec-ch-ua-mobile" -> "?0")
+
+  val headers_73 = Map(
+    "Accept" -> "application/vnd.uk.gov.hmcts.ccd-data-store-api.case-data-validate.v2+json;charset=UTF-8",
+    "Accept-Encoding" -> "gzip, deflate, br",
+    "Accept-Language" -> "en-GB,en-US;q=0.9,en;q=0.8",
+    "Content-Type" -> "application/json",
+    "Origin" -> "https://manage-case.demo.platform.hmcts.net",
+    "Request-Context" -> "appId=cid-v1:97eb6d07-2397-40dd-a595-917d825a8fdc",
+    "Request-Id" -> "|A3zwi.PmPOQ",
+    "Sec-Fetch-Dest" -> "empty",
+    "Sec-Fetch-Mode" -> "cors",
+    "Sec-Fetch-Site" -> "same-origin",
+    "X-XSRF-TOKEN" -> "9Fk6daby-kocuiVx4odgQKYNMpoq_0V8lX6E",
+    "experimental" -> "true",
+    "sec-ch-ua" -> """Chromium";v="86", "\"Not\\A;Brand";v="99", "Google Chrome";v="86""",
+    "sec-ch-ua-mobile" -> "?0")
+
+  val headers_75 = Map(
+    "Accept" -> "",
+    "Accept-Encoding" -> "gzip, deflate, br",
+    "Accept-Language" -> "en-GB,en-US;q=0.9,en;q=0.8",
+    "Content-Type" -> "multipart/form-data; boundary=----WebKitFormBoundarywubfdiKzsZsxAbvp",
+    "Origin" -> "https://manage-case.demo.platform.hmcts.net",
+    "Request-Context" -> "appId=cid-v1:97eb6d07-2397-40dd-a595-917d825a8fdc",
+    "Request-Id" -> "|A3zwi.Hzg3x",
+    "Sec-Fetch-Dest" -> "empty",
+    "Sec-Fetch-Mode" -> "cors",
+    "Sec-Fetch-Site" -> "same-origin",
+    "X-XSRF-TOKEN" -> "qYniqr2t-KcDI6l-bS9XcKPDfp-Lr9Q3hIc0",
+    "sec-ch-ua" -> """Chromium";v="86", "\"Not\\A;Brand";v="99", "Google Chrome";v="86""",
+    "sec-ch-ua-mobile" -> "?0")
+
+  val headers_76 = Map(
+    "Accept" -> "application/vnd.uk.gov.hmcts.ccd-data-store-api.case-data-validate.v2+json;charset=UTF-8",
+    "Accept-Encoding" -> "gzip, deflate, br",
+    "Accept-Language" -> "en-GB,en-US;q=0.9,en;q=0.8",
+    "Content-Type" -> "application/json",
+    "Origin" -> "https://manage-case.demo.platform.hmcts.net",
+    "Request-Context" -> "appId=cid-v1:97eb6d07-2397-40dd-a595-917d825a8fdc",
+    "Request-Id" -> "|A3zwi.sLaGm",
+    "Sec-Fetch-Dest" -> "empty",
+    "Sec-Fetch-Mode" -> "cors",
+    "Sec-Fetch-Site" -> "same-origin",
+    "X-XSRF-TOKEN" -> "aBLuy2nG-AQDJcwpsgT-hNmVgn_zkyVA86UU",
+    "experimental" -> "true",
+    "sec-ch-ua" -> """Chromium";v="86", "\"Not\\A;Brand";v="99", "Google Chrome";v="86""",
+    "sec-ch-ua-mobile" -> "?0")
+
+  val headers_78 = Map(
+    "Accept" -> "application/vnd.uk.gov.hmcts.ccd-data-store-api.case-data-validate.v2+json;charset=UTF-8",
+    "Accept-Encoding" -> "gzip, deflate, br",
+    "Accept-Language" -> "en-GB,en-US;q=0.9,en;q=0.8",
+    "Content-Type" -> "application/json",
+    "Origin" -> "https://manage-case.demo.platform.hmcts.net",
+    "Request-Context" -> "appId=cid-v1:97eb6d07-2397-40dd-a595-917d825a8fdc",
+    "Request-Id" -> "|A3zwi.QVv1/",
+    "Sec-Fetch-Dest" -> "empty",
+    "Sec-Fetch-Mode" -> "cors",
+    "Sec-Fetch-Site" -> "same-origin",
+    "X-XSRF-TOKEN" -> "dTaBXfeC-YhBq9rBjZfKP6ENdjgXpVSXa0Jo",
+    "experimental" -> "true",
+    "sec-ch-ua" -> """Chromium";v="86", "\"Not\\A;Brand";v="99", "Google Chrome";v="86""",
+    "sec-ch-ua-mobile" -> "?0")
+
+  val headers_81 = Map(
+    "Accept" -> "application/vnd.uk.gov.hmcts.ccd-data-store-api.case-data-validate.v2+json;charset=UTF-8",
+    "Accept-Encoding" -> "gzip, deflate, br",
+    "Accept-Language" -> "en-GB,en-US;q=0.9,en;q=0.8",
+    "Content-Type" -> "application/json",
+    "Origin" -> "https://manage-case.demo.platform.hmcts.net",
+    "Request-Context" -> "appId=cid-v1:97eb6d07-2397-40dd-a595-917d825a8fdc",
+    "Request-Id" -> "|A3zwi.VK8TT",
+    "Sec-Fetch-Dest" -> "empty",
+    "Sec-Fetch-Mode" -> "cors",
+    "Sec-Fetch-Site" -> "same-origin",
+    "X-XSRF-TOKEN" -> "7BlFjev6-ZIu_RVPAwBSktydWWESEOieLTG8",
+    "experimental" -> "true",
+    "sec-ch-ua" -> """Chromium";v="86", "\"Not\\A;Brand";v="99", "Google Chrome";v="86""",
+    "sec-ch-ua-mobile" -> "?0")
+
+  val headers_83 = Map(
+    "Accept" -> "application/vnd.uk.gov.hmcts.ccd-data-store-api.ui-user-profile.v2+json;charset=UTF-8",
+    "Accept-Encoding" -> "gzip, deflate, br",
+    "Accept-Language" -> "en-GB,en-US;q=0.9,en;q=0.8",
+    "Content-Type" -> "application/json",
+    "Request-Context" -> "appId=cid-v1:97eb6d07-2397-40dd-a595-917d825a8fdc",
+    "Request-Id" -> "|A3zwi.XPbbH",
+    "Sec-Fetch-Dest" -> "empty",
+    "Sec-Fetch-Mode" -> "cors",
+    "Sec-Fetch-Site" -> "same-origin",
+    "X-XSRF-TOKEN" -> "wSLj5vog--976Ofd4t15VQtreaMu1n6Yqm4o",
+    "experimental" -> "true",
+    "sec-ch-ua" -> """Chromium";v="86", "\"Not\\A;Brand";v="99", "Google Chrome";v="86""",
+    "sec-ch-ua-mobile" -> "?0")
+
+  val headers_84 = Map(
+    "Accept" -> "application/json, text/plain, */*",
+    "Accept-Encoding" -> "gzip, deflate, br",
+    "Accept-Language" -> "en-GB,en-US;q=0.9,en;q=0.8",
+    "Request-Context" -> "appId=cid-v1:97eb6d07-2397-40dd-a595-917d825a8fdc",
+    "Request-Id" -> "|A3zwi.wIgaM",
+    "Sec-Fetch-Dest" -> "empty",
+    "Sec-Fetch-Mode" -> "cors",
+    "Sec-Fetch-Site" -> "same-origin",
+    "sec-ch-ua" -> """Chromium";v="86", "\"Not\\A;Brand";v="99", "Google Chrome";v="86""",
+    "sec-ch-ua-mobile" -> "?0")
+
+  val headers_87 = Map(
+    "Accept" -> "application/vnd.uk.gov.hmcts.ccd-data-store-api.create-case.v2+json;charset=UTF-8",
+    "Accept-Encoding" -> "gzip, deflate, br",
+    "Accept-Language" -> "en-GB,en-US;q=0.9,en;q=0.8",
+    "Content-Type" -> "application/json",
+    "Origin" -> "https://manage-case.demo.platform.hmcts.net",
+    "Request-Context" -> "appId=cid-v1:97eb6d07-2397-40dd-a595-917d825a8fdc",
+    "Request-Id" -> "|A3zwi.0C6LD",
+    "Sec-Fetch-Dest" -> "empty",
+    "Sec-Fetch-Mode" -> "cors",
+    "Sec-Fetch-Site" -> "same-origin",
+    "X-XSRF-TOKEN" -> "FvNh0SVh-lD6EZoQw-FA4n-qSl7ZsYdvt6n0",
+    "experimental" -> "true",
+    "sec-ch-ua" -> """Chromium";v="86", "\"Not\\A;Brand";v="99", "Google Chrome";v="86""",
+    "sec-ch-ua-mobile" -> "?0")
+
+  val headers_89 = Map(
+    "Accept" -> "application/vnd.uk.gov.hmcts.ccd-data-store-api.ui-case-view.v2+json",
+    "Accept-Encoding" -> "gzip, deflate, br",
+    "Accept-Language" -> "en-GB,en-US;q=0.9,en;q=0.8",
+    "Content-Type" -> "application/json",
+    "Request-Context" -> "appId=cid-v1:97eb6d07-2397-40dd-a595-917d825a8fdc",
+    "Request-Id" -> "|A3zwi.0sXLb",
+    "Sec-Fetch-Dest" -> "empty",
+    "Sec-Fetch-Mode" -> "cors",
+    "Sec-Fetch-Site" -> "same-origin",
+    "X-XSRF-TOKEN" -> "s1tq7FBU-jQWTftZXAouibip2ywJ01i-0nPg",
+    "experimental" -> "true",
+    "sec-ch-ua" -> """Chromium";v="86", "\"Not\\A;Brand";v="99", "Google Chrome";v="86""",
+    "sec-ch-ua-mobile" -> "?0")
+
+  val headers_92 = Map(
+    "Accept" -> "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "Accept-Encoding" -> "gzip, deflate, br",
+    "Accept-Language" -> "en-GB,en-US;q=0.9,en;q=0.8",
+    "Sec-Fetch-Dest" -> "document",
+    "Sec-Fetch-Mode" -> "navigate",
+    "Sec-Fetch-Site" -> "same-origin",
+    "Sec-Fetch-User" -> "?1",
+    "Upgrade-Insecure-Requests" -> "1",
+    "sec-ch-ua" -> """Chromium";v="86", "\"Not\\A;Brand";v="99", "Google Chrome";v="86""",
+    "sec-ch-ua-mobile" -> "?0")
+
+}
