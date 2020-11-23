b45e43b37101e2466c208d0494a89e924a1f32e8
vijaykanth6 <32548931+vijaykanth6@users.noreply.github.com>
2020-11-23 18:07:46 +0000

    Update Environment.scala

--- src/gatling/simulations/uk/gov/hmcts/reform/exui/performance/scenarios/utils/Environment.scala
+++ src/gatling/simulations/uk/gov/hmcts/reform/exui/performance/scenarios/utils/Environment.scala
@@ -22,21 +22,21 @@ val baseDomainOrg="manage-org.perftest.platform.hmcts.net"
   val adminPasswordAO = "Monday01"
   val notificationClient="sidam_perftest-b7ab8862-25b4-41c9-8311-cb78815f7d2d-ebb113ff-da17-4646-a39e-f93783a993f4"
 
-  val minThinkTime = 30//100
+ val minThinkTime = 100
   //10
-  val maxThinkTime = 30//110
+  val maxThinkTime = 110
   //30
-  val minThinkTimeFPLC = 30//75
+  val minThinkTimeFPLC = 60//75
   //10
-  val maxThinkTimeFPLC = 32//80
+  val maxThinkTimeFPLC = 65//80
 
   val minThinkTimeSDO = 100
   //10
   val maxThinkTimeSDO = 110
   //30
-  val minThinkTimeIACC = 40//100
+  val minThinkTimeIACC = 90//100
   //10
-  val maxThinkTimeIACC = 42//120
+  val maxThinkTimeIACC = 100//120
   val minThinkTimeFPLV = 450
   //10
   val maxThinkTimeFPLV = 480
@@ -44,59 +44,21 @@ val baseDomainOrg="manage-org.perftest.platform.hmcts.net"
   val minThinkTimeIACV = 470
   //10
   val maxThinkTimeIACV = 490
-  val minThinkTimePROB = 180//300
-  val maxThinkTimePROB = 200//310
+  val minThinkTimePROB = 340//300
+  val maxThinkTimePROB = 360//310
 
-  val minThinkTimeDIV = 60//140
-  val maxThinkTimeDIV = 62//160
+  val minThinkTimeDIV = 100//140
+  val maxThinkTimeDIV = 110//160
 
-  val minThinkTimeCW = 20//50
-  val maxThinkTimeCW = 22//50
+  val minThinkTimeCW = 40//50
+  val maxThinkTimeCW = 42//50
 
-  val minThinkTimeFR =45//  100
-  val maxThinkTimeFR =48//100
+  val minThinkTimeFR =90//  100
+  val maxThinkTimeFR =95//48//100
 
   val constantthinkTime = 5
   val HttpProtocol = http
-
-// }
- //else {
- //================================================================================
- //below properties are for aat
- //================================================================================
- /* val url_approve = "https://administer-orgs.aat.platform.hmcts.net"
-  val manageOrdURL = "https://manage-org.aat.platform.hmcts.net"
-  val idamURL = "https://idam-web-public.aat.platform.hmcts.net"
-  val baseURL = "https://manage-case.aat.platform.hmcts.net"
-  val baseFPLAURL = "https://manage-case.aat.platform.hmcts.net"
-  val baseDomain="manage-case.aat.platform.hmcts.net"
-  val adminUserAO = "ao.admin.aat@mailinator.com"
-
-  val adminPasswordAO = "Pass19word"
- val notificationClient="sidam_aat-b7ab8862-25b4-41c9-8311-cb78815f7d2d-4ceaf178-56c6-4b78-988b-05e5fee73188"
-  val minThinkTime = 5
-  //10
-  val maxThinkTime = 6
-  //30
-  val minThinkTimeFPLC = 10
-  //10
-  val maxThinkTimeFPLC = 12
-  //30
-  val minThinkTimeIACC = 10
-  //10
-  val maxThinkTimeIACC = 12
-  val minThinkTimeFPLV = 10
-  //10
-  val maxThinkTimeFPLV = 12
-  //320
-  val minThinkTimeIACV = 10
-  //10
-  val maxThinkTimeIACV = 12
-  val minThinkTimePROB = 10
-  //10
-  val maxThinkTimePROB = 12
-  val constantthinkTime = 5
-  val HttpProtocol = http*/
+  
  //}
 
  val commonHeader = Map(
