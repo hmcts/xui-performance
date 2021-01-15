0b4f99eaae388077a50fc4df4f643f9ec41b79f5
vijayvykuntam <vijay.vykuntam@hmcts.net>
2021-01-15 13:16:03 +0000

    new release of EXUI

--- src/gatling/simulations/uk/gov/hmcts/reform/exui/performance/scenarios/EXUIProbateMC.scala
+++ src/gatling/simulations/uk/gov/hmcts/reform/exui/performance/scenarios/EXUIProbateMC.scala
@@ -50,7 +50,7 @@ object EXUIProbateMC {
 
       .pause(MinThinkTime , MaxThinkTime )
 
-
+//when click on create case tab
   val casecreation=group("XUI${service}_040_CreateCase") {
     exec(http("XUI${service}_040_CreateCase")
       .get("/aggregated/caseworkers/:uid/jurisdictions?access=create")
@@ -85,17 +85,14 @@ object EXUIProbateMC {
 
       .feed(Feeders.createCaseData)
       .group("XUI${service}_060_CreateApplication") {
-        exec(http("XUI${service}_060_CreateApplication")
+        exec(http("XUI${service}_060_005CreateApplication")
           .post("/data/case-types/GrantOfRepresentation/validate?pageId=solicitorCreateApplicationsolicitorCreateApplicationPage1")
           .headers(ProbateHeader.headers_casedata)
           .header("X-XSRF-TOKEN", "${XSRFToken}")
           .body(ElFileBody("RecordedSimulationPro1612_0048_request.json")).asJson
           .check(status.in(200, 304)))
-      }
-      .pause(MinThinkTime, MaxThinkTime)
 
-.group("XUI${service}_070_ApplicationDraft") {
-  exec(http("XUI${service}_070_005_ApplicationDraft")
+    .exec(http("XUI${service}_060_010_ApplicationDraft")
     .post("/data/internal/case-types/GrantOfRepresentation/drafts/")
     .headers(ProbateHeader.headers_draft)
     .header("X-XSRF-TOKEN", "${XSRFToken}")
@@ -106,8 +103,8 @@ object EXUIProbateMC {
 }
 .pause(MinThinkTime, MaxThinkTime)
 
-.group("XUI${service}_080_AddressLookup") {
-  exec(http("XUI${service}_080_AddressLookup")
+.group("XUI${service}_070_AddressLookup") {
+  exec(http("XUI${service}_070_AddressLookup")
     .get("/api/addresses?postcode=TW33SD")
     .headers(ProbateHeader.headers_28)
     .header("X-XSRF-TOKEN", "${XSRFToken}")
@@ -115,23 +112,23 @@ object EXUIProbateMC {
       }
       .pause(MinThinkTime, MaxThinkTime)
 
-.group("XUI${service}_090_CreateApplication2")
+.group("XUI${service}_080_CreateApplication2")
   {
-    exec(http("XUI${service}_090_005_CreateApplication2")
+    exec(http("XUI${service}_080_005_CreateApplication2")
       .post("/data/case-types/GrantOfRepresentation/validate?pageId=solicitorCreateApplicationsolicitorCreateApplicationPage2")
       .headers(ProbateHeader.headers_casedata)
       .header("X-XSRF-TOKEN", "${XSRFToken}")
       .body(ElFileBody("RecordedSimulationPro1612_0126_request.json")).asJson
       .check(status.in(200, 304)))
 
-    .exec(http("XUI${service}_090_010_Draft")
+    .exec(http("XUI${service}_080_010_Draft")
   .put("/data/internal/case-types/GrantOfRepresentation/drafts/${draftId}")
   .headers(ProbateHeader.headers_drfts)
   .header("X-XSRF-TOKEN", "${XSRFToken}")
   .body(ElFileBody("RecordedSimulationProbatecreate_0142_request.json")).asJson
   .check(status.in(400,200,201)))
 
-    .exec(http("XUI${service}_090_015_DraftProfile")
+    .exec(http("XUI${service}_080_015_DraftProfile")
       .get("/data/internal/profile")
       .headers(ProbateHeader.headers_casedataprofile)
       .header("X-XSRF-TOKEN", "${XSRFToken}")
@@ -139,15 +136,15 @@ object EXUIProbateMC {
 
   }.pause(MinThinkTime, MaxThinkTime)
 
-  .group("XUI${service}_100_CaseSubmitted") {
-    exec(http("XUI${service}_100_005_CaseSubmitted")
+  .group("XUI${service}_090_CaseSubmitted") {
+    exec(http("XUI${service}_090_005_CaseSubmitted")
       .post("/data/case-types/GrantOfRepresentation/cases?ignore-warning=false")    .headers(ProbateHeader.headers_solappcreated)
       .header("X-XSRF-TOKEN", "${XSRFToken}")
       .body(ElFileBody("RecordedSimulationPro1612_0144_request.json")).asJson
       .check(status.in(200, 304, 201))
       .check(jsonPath("$.id").optional.saveAs("caseId")))
       //below is for view the case details page with progression bar
-    .exec(http("XUI${service}_100_010_ViewCase")
+    .exec(http("XUI${service}_090_010_ViewCase")
         .get("/data/internal/cases/${caseId}")
         .headers(ProbateHeader.headers_saveandviewcase)
         .header("X-XSRF-TOKEN", "${XSRFToken}")
