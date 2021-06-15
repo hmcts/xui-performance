058169b97c0fdd5598ac86bb65f1d443a9f23d22
Jon Soloway <jon.soloway@hmcts.net>
2021-06-15 17:05:13 +0100

    General tidy up of XUI suite

--- src/gatling/simulations/uk/gov/hmcts/reform/exui/performance/scenarios/EXUIMCLogin.scala
+++ src/gatling/simulations/uk/gov/hmcts/reform/exui/performance/scenarios/EXUIMCLogin.scala
@@ -21,29 +21,29 @@ object EXUIMCLogin {
   *below requests are Homepage and relavant sub requests for Manage cases
   *=====================================================================================*/
   
-  val manageCasesHomePage =group("XUI${service}_010_Homepage") {
-    exec(http("XUI${service}_010_005_Homepage")
+  val manageCasesHomePage =group("XUI_${service}_010_Homepage") {
+    exec(http("XUI_${service}_010_005_Homepage")
       .get("/")
       .headers(LoginHeader.headers_0)
       .check(status.in(200, 304))).exitHereIfFailed
 
-    .exec(http("XUI${service}_010_010_HomepageConfigUI")
+    .exec(http("XUI_${service}_010_010_HomepageConfigUI")
       .get("/external/configuration-ui")
       .headers(LoginHeader.headers_1))
 
-    .exec(http("XUI${service}_010_015_HomepageConfigJson")
+    .exec(http("XUI_${service}_010_015_HomepageConfigJson")
       .get("/assets/config/config.json")
       .headers(LoginHeader.headers_1))
 
-    .exec(http("XUI${service}_010_020_HomepageTCEnabled")
+    .exec(http("XUI_${service}_010_020_HomepageTCEnabled")
       .get("/api/configuration?configurationKey=termsAndConditionsEnabled")
       .headers(LoginHeader.headers_1))
 
-    .exec(http("XUI${service}_010_025_HomepageIsAuthenticated")
+    .exec(http("XUI_${service}_010_025_HomepageIsAuthenticated")
       .get("/auth/isAuthenticated")
       .headers(LoginHeader.headers_1))
 
-    .exec(http("XUI${service}_010_030_AuthLogin")
+    .exec(http("XUI_${service}_010_030_AuthLogin")
       .get("/auth/login")
       .headers(LoginHeader.headers_4)
       .check(css("input[name='_csrf']", "value").saveAs("csrfToken"))
@@ -61,28 +61,28 @@ object EXUIMCLogin {
   
   val manageOrgHomePage =
     
-    exec(http("XUI${service}_010_005_Homepage")
+    exec(http("XUI_${service}_010_005_Homepage")
          .get(manageOrgURL + "/")
          .headers(LoginHeader.headers_0)
          .check(status.in(200,304))).exitHereIfFailed
     
-    .exec(http("XUI${service}_010_010_HomepageConfigUI")
+    .exec(http("XUI_${service}_010_010_HomepageConfigUI")
           .get(manageOrgURL + "/external/configuration-ui")
           .headers(LoginHeader.headers_1))
     
-    .exec(http("XUI${service}_010_015_HomepageConfigJson")
+    .exec(http("XUI_${service}_010_015_HomepageConfigJson")
           .get(manageOrgURL + "/assets/config/config.json")
           .headers(LoginHeader.headers_1))
     
-    .exec(http("XUI${service}_010_020_HomepageTCEnabled")
+    .exec(http("XUI_${service}_010_020_HomepageTCEnabled")
           .get(manageOrgURL + "/api/configuration?configurationKey=termsAndConditionsEnabled")
           .headers(LoginHeader.headers_1))
     
-    .exec(http("XUI${service}_010_025_HomepageIsAuthenticated")
+    .exec(http("XUI_${service}_010_025_HomepageIsAuthenticated")
           .get(manageOrgURL + "/auth/isAuthenticated")
           .headers(LoginHeader.headers_1))
     
-    .exec(http("XUI${service}_010_030_AuthLogin")
+    .exec(http("XUI_${service}_010_030_AuthLogin")
           .get(manageOrgURL + "/auth/login")
           .headers(LoginHeader.headers_4)
           .check(css("input[name='_csrf']", "value").saveAs("csrfToken"))
@@ -101,8 +101,8 @@ object EXUIMCLogin {
   
   val manageOrglogin =
     
-    group("XUI${service}_020_SignIn") {
-      exec(flushHttpCache).exec(http("XUI${service}_020_005_SignIn")
+    group("XUI_${service}_020_SignIn") {
+      exec(flushHttpCache).exec(http("XUI_${service}_020_005_SignIn")
             .post(IdamUrl + "/login?response_type=code&redirect_uri=https%3A%2F%2F" + orgDomain + "%2Foauth2%2Fcallback&scope=profile%20openid%20roles%20manage-user%20create-user%20manage-roles&state=${state}&client_id=xuimowebapp")
             .formParam("username", "${respuser}")
             .formParam("password", "Pass19word")
@@ -115,27 +115,27 @@ object EXUIMCLogin {
       .exec(getCookieValue(CookieKey("__auth__").withDomain(orgDomain).saveAs("authTokenResp")))
       .exec(getCookieValue(CookieKey("XSRF-TOKEN").withDomain(orgDomain).saveAs("XSRFToken")))
       
-      .exec(http("XUI${service}_020_010_Homepage")
+      .exec(http("XUI_${service}_020_010_Homepage")
             .get(manageOrgURL + "/external/config/ui")
             .headers(LoginHeader.headers_0)
             .check(status.in(200, 304)))
       
-      .exec(http("XUI${service}_020_015_Homepage")
+      .exec(http("XUI_${service}_020_015_Homepage")
             .get("/api/user/details")
             .headers(LoginHeader.headers_manageorglogin)
             .check(status.in(200, 304)))
       
-      .exec(http("XUI${service}_020_020_SignInTCEnabled")
+      .exec(http("XUI_${service}_020_020_SignInTCEnabled")
             .get("/auth/isAuthenticated")
             .headers(LoginHeader.headers_manageorglogin)
             .check(status.in(200, 304)))
       
-      .exec(http("XUI${service}_020_025_SignInTCEnabled")
+      .exec(http("XUI_${service}_020_025_SignInTCEnabled")
             .get("/external/configuration?configurationKey=feature.termsAndConditionsEnabled")
             .headers(LoginHeader.headers_manageorglogin)
             .check(status.in(200, 304)))
       
-      .exec(http("XUI${service}_020_030_APIOrg")
+      .exec(http("XUI_${service}_020_030_APIOrg")
             .get("/api/organisation")
             .headers(LoginHeader.headers_manageorglogin)
             .check(status.in(200, 304)))
@@ -153,8 +153,8 @@ object EXUIMCLogin {
   //The top row is for OAUTH2 and the bottom row is for OPIDC
   
   val manageCaseslogin =
-    group("XUI${service}_020_005_SignIn") {
-      exec(flushHttpCache).exec(http("XUI${service}_020_005_SignIn")
+    group("XUI_${service}_020_005_SignIn") {
+      exec(flushHttpCache).exec(http("XUI_${service}_020_005_SignIn")
                                 // .post(IdamUrl + "/login?response_type=code&redirect_uri=" + baseURL + "%2Foauth2%2Fcallback&scope=profile%20openid%20roles%20manage-user%20create-user&state=${state}&client_id=xuiwebapp")
                                 .post(IdamUrl + "/login?client_id=xuiwebapp&redirect_uri=" + baseURL + "/oauth2/callback&state=${state}&nonce=${nonce}&response_type=code&scope=profile%20openid%20roles%20manage-user%20create-user&prompt=")
                                 .formParam("username", "${user}")
@@ -169,42 +169,42 @@ object EXUIMCLogin {
       //following is the other way of getting cookies
       // .check(headerRegex("Set-Cookie","__auth-token=(.*)").saveAs("authToken"))
       
-      .exec(http("XUI${service}_020_010_configUI")
+      .exec(http("XUI_${service}_020_010_configUI")
             .get("/external/config/ui")
             .headers(LoginHeader.headers_0)
             .check(status.in(200, 304)))
       
-      .exec(http("XUI${service}_020_015_Config")
+      .exec(http("XUI_${service}_020_015_Config")
             .get("/assets/config/config.json")
             .headers(LoginHeader.headers_0)
             .check(status.in(200, 304)))
       
-      .exec(http("XUI${service}_020_020_SignInTCEnabled")
+      .exec(http("XUI_${service}_020_020_SignInTCEnabled")
             .get("/api/configuration?configurationKey=termsAndConditionsEnabled")
             .headers(LoginHeader.headers_38)
             .check(status.in(200, 304)))
       
-      .exec(http("XUI${service}_020_025_SignInGetUserId")
+      .exec(http("XUI_${service}_020_025_SignInGetUserId")
             .get("/api/user/details")
             .headers(LoginHeader.headers_0)
             .check(status.in(200, 304)))
       
       .repeat(1, "count") {
-        exec(http("XUI${service}_020_030_AcceptT&CAccessJurisdictions${count}")
+        exec(http("XUI_${service}_020_030_AcceptT&CAccessJurisdictions${count}")
              .get("/aggregated/caseworkers/:uid/jurisdictions?access=read")
              .headers(LoginHeader.headers_access_read)
              .check(status.in(200, 304, 302)))
       }
-      .exec(http("XUI${service}_020_035_GetWorkBasketInputs")
+      .exec(http("XUI_${service}_020_035_GetWorkBasketInputs")
             .get("/data/internal/case-types/FinancialRemedyMVP2/work-basket-inputs")
             .headers(LoginHeader.headers_17)
             .check(status.in(200, 304, 302)))
       
-      .exec(http("XUI${service}_020_040_HomepageIsAuthenticated")
+      .exec(http("XUI_${service}_020_040_HomepageIsAuthenticated")
             .get("/auth/isAuthenticated")
             .headers(LoginHeader.headers_0))
       
-      /*.exec(http("XUI${service}_020_045_GetDefaultWorkBasketView")
+      /*.exec(http("XUI_${service}_020_045_GetDefaultWorkBasketView")
             .get("/data/internal/searchCases?ctid=FinancialRemedyConsentedRespondent&use_case=WORKBASKET&view=WORKBASKET&state=caseAdded&page=1")
             .headers(LoginHeader.headers_0))*/
       .exec(getCookieValue(CookieKey("XSRF-TOKEN").withDomain(baseDomain).saveAs("XSRFToken")))
@@ -221,8 +221,8 @@ object EXUIMCLogin {
   //The top row is for OAUTH2 and the bottom row is for OPIDC
   
   val caseworkerLogin =
-    group("XUI${service}_020_SignIn") {
-      exec(http("XUI${service}_020_005_SignIn")
+    group("XUI_${service}_020_SignIn") {
+      exec(http("XUI_${service}_020_005_SignIn")
            //          .post(IdamUrl + "/login?response_type=code&redirect_uri=" + baseURL + "%2Foauth2%2Fcallback&scope=profile%20openid%20roles%20manage-user%20create-user&state=${state}&client_id=xuiwebapp")
            .post(IdamUrl + "/login?client_id=xuiwebapp&redirect_uri=" + baseURL + "/oauth2/callback&state=${state}&nonce=${nonce}&response_type=code&scope=profile%20openid%20roles%20manage-user%20create-user&prompt=")
            .formParam("username", "${user}")
@@ -235,29 +235,29 @@ object EXUIMCLogin {
       //      .check(regex("Manage Cases")))
            .exitHereIfFailed
       
-      .exec(http("XUI${service}_020_010_Homepage")
+      .exec(http("XUI_${service}_020_010_Homepage")
             .get("/external/config/ui")
             .headers(LoginHeader.headers_0)
             .check(status.in(200, 304)))
       
-      .exec(http("XUI${service}_020_015_SignInTCEnabled")
+      .exec(http("XUI_${service}_020_015_SignInTCEnabled")
             .get("/api/configuration?configurationKey=termsAndConditionsEnabled")
             .headers(LoginHeader.headers_38)
             .check(status.in(200, 304)))
       
       .repeat(1, "count") {
-        exec(http("XUI${service}_020_020_AcceptT&CAccessJurisdictions${count}")
+        exec(http("XUI_${service}_020_020_AcceptT&CAccessJurisdictions${count}")
              .get("/aggregated/caseworkers/:uid/jurisdictions?access=read")
              .headers(LoginHeader.headers_access_read)
              .check(status.in(200, 304, 302)))
       }
       
-      .exec(http("XUI${service}_020_025_GetWorkBasketInputs")
+      .exec(http("XUI_${service}_020_025_GetWorkBasketInputs")
             .get("/data/internal/case-types/GrantOfRepresentation/work-basket-inputs")
             .headers(LoginHeader.headers_17))
       
       
-     /* .exec(http("XUI${service}_020_030_GetDefaultWorkBasketView")
+     /* .exec(http("XUI_${service}_020_030_GetDefaultWorkBasketView")
             .get("/data/internal/searchCases?ctid=FinancialRemedyConsentedRespondent&use_case=WORKBASKET&view=WORKBASKET&state=caseAdded&page=1")
             .headers(LoginHeader.headers_0))*/
       
@@ -276,33 +276,33 @@ object EXUIMCLogin {
   
   val termsnconditions=
     
-    exec(http("XUI${service}_030_005_ConfirmT&C")
+    exec(http("XUI_${service}_030_005_ConfirmT&C")
          .post("/api/userTermsAndConditions")
          .headers(LoginHeader.headers_tc)
          .body(StringBody("{\"userId\":\"${myUserId}\"}"))
          .check(status.in(200, 304, 302))).exitHereIfFailed
     
-    .exec(http("XUI${service}_030_010_AcceptT&CEnabled")
+    .exec(http("XUI_${service}_030_010_AcceptT&CEnabled")
           .get("/api/configuration?configurationKey=termsAndConditionsEnabled")
           .headers(LoginHeader.headers_hometc)
           .check(status.in(200, 304, 302)))
     
     .repeat(1, "count") {
-      exec(http("XUI${service}_030_015_AcceptT&CAccessJurisdictions${count}")
+      exec(http("XUI_${service}_030_015_AcceptT&CAccessJurisdictions${count}")
            .get("/aggregated/caseworkers/:uid/jurisdictions?access=read")
            .headers(LoginHeader.headers_access_read)
            .check(status.in(200, 304, 302)))
     }
     
-    .exec(http("XUI${service}_030_020_GetWorkBasketInputs")
+    .exec(http("XUI_${service}_030_020_GetWorkBasketInputs")
           .get("/data/internal/case-types/CARE_SUPERVISION_EPO/work-basket-inputs")
           .headers(LoginHeader.headers_17))
     
-    .exec(http("XUI${service}_030_025_GetPaginationMetaData")
+    .exec(http("XUI_${service}_030_025_GetPaginationMetaData")
           .get("/data/caseworkers/:uid/jurisdictions/PUBLICLAW/case-types/CARE_SUPERVISION_EPO/cases/pagination_metadata?state=Open")
           .headers(LoginHeader.headers_0))
     
-    .exec(http("XUI${service}_030_030_GetDefaultWorkBasketView")
+    .exec(http("XUI_${service}_030_030_GetDefaultWorkBasketView")
           .get("/aggregated/caseworkers/:uid/jurisdictions/PUBLICLAW/case-types/CARE_SUPERVISION_EPO/cases?view=WORKBASKET&state=Open&page=1")
           .headers(LoginHeader.headers_0))
     
@@ -313,7 +313,7 @@ object EXUIMCLogin {
   val managecasesadminlogin =
     
     
-    exec(http("XUI${service}_020_005_SignIn")
+    exec(http("XUI_${service}_020_005_SignIn")
          .post(IdamUrl + "/login?response_type=code&client_id=xuiwebapp&redirect_uri=" + baseURL + "/oauth2/callback&scope=profile%20openid%20roles%20manage-user%20create-user")
          .formParam("username", "fpla.admin@mailinator.com")
          .formParam("password", "Pass19word")
@@ -323,32 +323,32 @@ object EXUIMCLogin {
          .headers(LoginHeader.headers_login_submit)
          .check(status.in(200, 304, 302))).exitHereIfFailed
     
-    .exec(http("XUI${service}_020_010_Homepage")
+    .exec(http("XUI_${service}_020_010_Homepage")
           .get("/external/config/ui")
           .headers(LoginHeader.headers_0)
           .check(status.in(200,304)))
     
-    .exec(http("XUI${service}_020_015_SignInTCEnabled")
+    .exec(http("XUI_${service}_020_015_SignInTCEnabled")
           .get("/api/configuration?configurationKey=termsAndConditionsEnabled")
           .headers(LoginHeader.headers_38)
           .check(status.in(200, 304)))
     
     .repeat(1, "count") {
-      exec(http("XUI${service}_020_020_AcceptT&CAccessJurisdictions${count}")
+      exec(http("XUI_${service}_020_020_AcceptT&CAccessJurisdictions${count}")
            .get("/aggregated/caseworkers/:uid/jurisdictions?access=read")
            .headers(LoginHeader.headers_access_read)
            .check(status.in(200, 304, 302)))
     }
     
-    .exec(http("XUI${service}_020_025_GetWorkBasketInputs")
+    .exec(http("XUI_${service}_020_025_GetWorkBasketInputs")
           .get("/data/internal/case-types/CARE_SUPERVISION_EPO/work-basket-inputs")
           .headers(LoginHeader.headers_17))
     
-    .exec(http("XUI${service}_020_030_GetPaginationMetaData")
+    .exec(http("XUI_${service}_020_030_GetPaginationMetaData")
           .get("/data/caseworkers/:uid/jurisdictions/PUBLICLAW/case-types/CARE_SUPERVISION_EPO/cases/pagination_metadata?state=Open")
           .headers(LoginHeader.headers_0))
     
-    .exec(http("XUI${service}_020_035_GetDefaultWorkBasketView")
+    .exec(http("XUI_${service}_020_035_GetDefaultWorkBasketView")
           .get("/aggregated/caseworkers/:uid/jurisdictions/PUBLICLAW/case-types/CARE_SUPERVISION_EPO/cases?view=WORKBASKET&state=Open&page=1")
           .headers(LoginHeader.headers_0))
     
@@ -359,7 +359,7 @@ object EXUIMCLogin {
   val managecasesgatekeeperlogin =
     
     
-    exec(http("XUI${service}_170_005_GKSignIn")
+    exec(http("XUI_${service}_170_005_GKSignIn")
          .post(IdamUrl + "/login?response_type=code&client_id=xuiwebapp&redirect_uri=" + baseURL + "/oauth2/callback&scope=profile%20openid%20roles%20manage-user%20create-user")
          .formParam("username", "fpla.gatekeeper@mailinator.com")
          .formParam("password", "Pass19word")
@@ -369,32 +369,32 @@ object EXUIMCLogin {
          .headers(LoginHeader.headers_login_submit)
          .check(status.in(200, 304, 302))).exitHereIfFailed
     
-    .exec(http("XUI${service}_170_010_GKSignIn")
+    .exec(http("XUI_${service}_170_010_GKSignIn")
           .get("/external/config/ui")
           .headers(LoginHeader.headers_0)
           .check(status.in(200,304)))
     
-    .exec(http("XUI${service}_170_015_GKSignInTCEnabled")
+    .exec(http("XUI_${service}_170_015_GKSignInTCEnabled")
           .get("/api/configuration?configurationKey=termsAndConditionsEnabled")
           .headers(LoginHeader.headers_38)
           .check(status.in(200, 304)))
     
     .repeat(1, "count") {
-      exec(http("XUI${service}_170_020_GKAcceptT&CAccessJurisdictions${count}")
+      exec(http("XUI_${service}_170_020_GKAcceptT&CAccessJurisdictions${count}")
            .get("/aggregated/caseworkers/:uid/jurisdictions?access=read")
            .headers(LoginHeader.headers_access_read)
            .check(status.in(200, 304, 302)))
     }
     
-    .exec(http("XUI${service}_170_025_GKGetWorkBasketInputs")
+    .exec(http("XUI_${service}_170_025_GKGetWorkBasketInputs")
           .get("/data/internal/case-types/CARE_SUPERVISION_EPO/work-basket-inputs")
           .headers(LoginHeader.headers_17))
     
-    .exec(http("XUI${service}_170_030_GKGetPaginationMetaData")
+    .exec(http("XUI_${service}_170_030_GKGetPaginationMetaData")
           .get("/data/caseworkers/:uid/jurisdictions/PUBLICLAW/case-types/CARE_SUPERVISION_EPO/cases/pagination_metadata?state=Open")
           .headers(LoginHeader.headers_0))
     
-    .exec(http("XUI${service}_170_035_GKGetDefaultWorkBasketView")
+    .exec(http("XUI_${service}_170_035_GKGetDefaultWorkBasketView")
           .get("/aggregated/caseworkers/:uid/jurisdictions/PUBLICLAW/case-types/CARE_SUPERVISION_EPO/cases?view=WORKBASKET&state=Open&page=1")
           .headers(LoginHeader.headers_0))
     
@@ -407,8 +407,8 @@ object EXUIMCLogin {
   //below requests are Terms and Conditions page and relavant sub requests
   // ======================================================================================
   
-  val manageCase_Logout =group("XUI${service}_${SignoutNumber}_SignOut") {
-    exec(http("XUI${service}_${SignoutNumber}_SignOut")
+  val manageCase_Logout =group("XUI_${service}_999_SignOut") {
+    exec(http("XUI_${service}_999_SignOut")
          .get("/api/logout")
          .headers(LoginHeader.headers_signout)
          .check(status.in(200, 304, 302)))
@@ -416,7 +416,7 @@ object EXUIMCLogin {
   
   val manageOrg_Logout =
     
-    exec(http("XUI${service}_${SignoutNumber}_SignOut")
+    exec(http("XUI_${service}_999_SignOut")
          .get(manageOrgURL + "/api/logout")
          .headers(LoginHeader.headers_signout)
          .check(status.in(200, 304, 302)))
@@ -424,7 +424,7 @@ object EXUIMCLogin {
   
   val manageCase_LogoutAdmin =
     
-    exec(http("XUI${service}_${SignoutNumberAdmin}_SignOut")
+    exec(http("XUI_${service}_999_SignOut")
          .get("/api/logout")
          .headers(LoginHeader.headers_signout)
          .check(status.in(200, 304, 302)))
@@ -433,7 +433,7 @@ object EXUIMCLogin {
   
   val manageCase_LogoutGK =
     
-    exec(http("XUI${service}_${SignoutNumberGK}_SignOut")
+    exec(http("XUI_${service}_999_SignOut")
          .get("/api/logout")
          .headers(LoginHeader.headers_signout)
          .check(status.in(200, 304, 302)))
