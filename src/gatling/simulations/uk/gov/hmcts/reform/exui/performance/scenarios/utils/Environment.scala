0cb2898547c646b6a3c79f4fdccd7898f3de42ce
Jon Soloway <jon.soloway@hmcts.net>
2021-06-25 11:37:16 +0100

    Updating probate thinktime

--- src/gatling/simulations/uk/gov/hmcts/reform/exui/performance/scenarios/utils/Environment.scala
+++ src/gatling/simulations/uk/gov/hmcts/reform/exui/performance/scenarios/utils/Environment.scala
@@ -41,8 +41,8 @@ val baseDomainOrg="manage-org.perftest.platform.hmcts.net"
   val minThinkTimeIACV = 10//470
   //10
   val maxThinkTimeIACV = 10//490
-  val minThinkTimePROB = 25 //180
-  val maxThinkTimePROB = 25 //190
+  val minThinkTimePROB = 35 //180
+  val maxThinkTimePROB = 35 //190
 
   val minThinkTimeDIV = 60
   val maxThinkTimeDIV = 60
