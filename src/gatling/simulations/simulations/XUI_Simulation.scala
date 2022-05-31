b5beb6e41bbb72fe5cd9f431b89bf52432fa0151
Jack <jack.saville@hmcts.net>
2022-05-31 14:02:58 +0100

    Space fixes

--- src/gatling/simulations/simulations/XUI_Simulation.scala
+++ src/gatling/simulations/simulations/XUI_Simulation.scala
@@ -64,7 +64,6 @@ class XUI_Simulation extends Simulation {
 	//This determines the percentage split of PRL journeys, by C100 or FL401
 	val prlC100Percentage = 66 //Percentage of C100s (the rest will be FL401s) - should be 66 for the 2:1 ratio
 
-
 	val rampUpDurationMins = 5
 	val rampDownDurationMins = 5
 	val testDurationMins = 60
