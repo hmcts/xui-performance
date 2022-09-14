9ab8ae15e7c055c6b8f342ca0c574330181d87f3
Jack <jack.saville@hmcts.net>
2022-09-14 10:48:48 +0100

    Test ready

--- src/gatling/simulations/simulations/XUI_Simulation.scala
+++ src/gatling/simulations/simulations/XUI_Simulation.scala
@@ -226,7 +226,7 @@ class XUI_Simulation extends Simulation {
 					.set("caseType", "Benefit"))
 				.exec(Homepage.XUIHomePage)
 				.exec(Login.XUILogin)
-				.repeat(4) { //5, 1st year = 4
+				.repeat(5) { //5, 1st year = 4
 					repeat(6) { //6
 						exec(Solicitor_Hearings.ViewAllHearings)
 							//	.exec(Solicitor_Hearings.AmendHearing)
@@ -241,7 +241,7 @@ class XUI_Simulation extends Simulation {
 						.exec(Solicitor_Hearings.RequestHearing)
 		//				.exec(Solicitor_Hearings.UploadResponse)
 				.exec(Solicitor_Hearings.UpdateHearing)
-				.repeat(14) {//13, first year = 14
+				.repeat(13) {//13, first year = 14
 					exec(Solicitor_Hearings.ViewAllHearings)
 						.exec(Solicitor_Hearings.GetHearing)
 				}
@@ -550,10 +550,10 @@ class XUI_Simulation extends Simulation {
 //		.assertions(assertions(testType))
 //		.maxDuration(60 minutes)
 
-	setUp(HearingsScenario.inject(rampUsers(32).during(11880)))
+	setUp(HearingsScenario.inject(rampUsers(10).during(1600)))
 	// (RUDH.inject(rampUsers(250).during(3200))))
 	   .protocols(httpProtocol)
-		.maxDuration(20000)
+		.maxDuration(4500)
 	  // .maxDuration(20000)
 
 
