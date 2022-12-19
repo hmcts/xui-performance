58aee57a58e1298abdff37f06cd3bd110f6ee1da
Jack <jack.saville@hmcts.net>
2022-12-19 13:47:25 +0000

    data prep ready

--- src/gatling/simulations/simulations/XUI_Simulation.scala
+++ src/gatling/simulations/simulations/XUI_Simulation.scala
@@ -105,7 +105,7 @@ class XUI_Simulation extends Simulation {
 	val PRLSolicitorScenario = scenario("***** Private Law Create Case *****")
 		.exitBlockOnFail {
 		//	.repeat(5) {
-			feed(UserFeederPRL2)
+			feed(UserFeederPRL)
 				.exec(_.set("env", s"${env}")
 					.set("caseType", "PRLAPPS"))
 				.exec(Homepage.XUIHomePage)
@@ -114,7 +114,7 @@ class XUI_Simulation extends Simulation {
 				.doIfOrElse(session => session("prl-percentage").as[Int] < prlC100Percentage) {
 				//	.repeat(3) {
 						//C100 Journey
-/*
+
 						exec(Solicitor_PRL_C100.CreatePrivateLawCase)
 							.exec(Solicitor_PRL_C100.TypeOfApplication)
 							.exec(Solicitor_PRL_C100.HearingUrgency)
@@ -128,7 +128,6 @@ class XUI_Simulation extends Simulation {
 
 
 
- */
 
 
 
@@ -138,7 +137,8 @@ class XUI_Simulation extends Simulation {
 
 
 
-						exec(Solicitor_PRL_AddAnOrder.AddAnOrder)
+
+					//	exec(Solicitor_PRL_AddAnOrder.AddAnOrder)
 						//	.exec(Solicitor_PRL_Continued.PRL)
 
 
