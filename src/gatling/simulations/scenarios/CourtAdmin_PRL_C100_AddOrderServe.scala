cdf76c2665bb70975b18d7bc7269130656876a92
Dean Cullen <Dean_cullen@next.co.uk>
2024-07-16 15:41:14 +0100

    updates

--- src/gatling/simulations/scenarios/CourtAdmin_PRL_C100_AddOrderServe.scala
+++ src/gatling/simulations/scenarios/CourtAdmin_PRL_C100_AddOrderServe.scala
@@ -210,6 +210,10 @@ object CourtAdmin_PRL_C100_AddOrderServe {
         .headers(Headers.commonHeader)
         .header("accept", "application/vnd.uk.gov.hmcts.ccd-data-store-api.ui-start-event-trigger.v2+json;charset=UTF-8")
         .check(jsonPath("$.event_token").saveAs("event_token"))
+        .check(jsonPath("$.case_fields[1].value[0].value.whoDoesTheChildLiveWith.list_items[0].code").findAll.saveAs("liveWithListItemsCode"))
+        .check(jsonPath("$.case_fields[1].value[0].value.whoDoesTheChildLiveWith.list_items[0].label").findAll.saveAs("liveWithListItemsLabel"))
+        .check(jsonPath("$.case_fields[1].value[0].value.whoDoesTheChildLiveWith.list_items").findAll.saveAs("liveWithListItems"))
+        .check(jsonPath("$.case_fields[1].value[0].id").saveAs("childDetailsID"))
         .check(jsonPath("$.id").is("childDetailsRevised")))
 
     .pause(MinThinkTime, MaxThinkTime)
