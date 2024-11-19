6c20bab36257f3ae4f52c7e483d8238bde2fe2b6
jmcadam <mcadam88@gmail.com>
2024-11-19 16:35:37 +0000

    update e2e assertions

--- src/gatling/simulations/simulations/XUI_Simulation.scala
+++ src/gatling/simulations/simulations/XUI_Simulation.scala
@@ -548,12 +548,11 @@ class XUI_Simulation extends Simulation {
 			case "perftest" | "pipeline" => //currently using the same assertions for a performance test and the pipeline
 				if (debugMode == "off") {
 					Seq(global.successfulRequests.percent.gte(95),
-						details("XUI_PRL_C100_460_SubmitAndPayNow").successfulRequests.percent.gte(80),
+						details("XUI_PRL_C100_620_SubmitAndPayNow").successfulRequests.percent.gte(80),
 						details("XUI_PRL_FL401_490_SOTSubmit").successfulRequests.percent.gte(80),
-						details("XUI_Probate_300_ViewCase").successfulRequests.percent.gte(80),
-						details("XUI_IAC_310_ShareACaseConfirm").successfulRequests.percent.gte(80),
+						details("XUI_Probate_330_ViewCase").successfulRequests.percent.gte(80),
+						details("XUI_IAC_280_005_AppealDeclarationSubmitted").successfulRequests.percent.gte(80),
 						details("XUI_FPL_330_ReturnToCase").successfulRequests.percent.gte(80),
-						details("XUI_Divorce_270_SubmitPetition").successfulRequests.percent.gte(80),
 						details("XUI_000_CCDEvent-system-progress-case-awaiting-final-order").successfulRequests.percent.gte(80), //NFD Sole
 						details("XUI_000_CCDEvent-system-progress-held-case").successfulRequests.percent.gte(80), //NFD Joint
 						details("XUI_FR_170_SubmitApplication").successfulRequests.percent.gte(80),
