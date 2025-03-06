ac8961b6bbf50396c547f93c48f5632eb132038d
Jon Soloway <jon.soloway@hmcts.net>
2025-03-06 09:01:31 +0000

    Updating test duration

--- src/gatling/simulations/utils/Config.scala
+++ src/gatling/simulations/utils/Config.scala
@@ -10,7 +10,7 @@ object Config {
   /* PED CONFIG */
   val PED_WRITE_LOGS_TO_FILES = true //Users will write messages to files, so response times can be calculated
   val PED_LOG_OUTPUT_PATH = "logs/ped/" + TEST_DATE_TIME
-  val PED_PRESENTATION_DURATION_MINS = 1 //The duration for which Presenters will send messages
+  val PED_PRESENTATION_DURATION_MINS = 20 //The duration for which Presenters will send messages
   val PED_SEND_MESSAGE_FREQ_MS = 500 //Send messages every 500ms
   val PED_POLL_MESSAGES_FREQ_MS = 10000 //Poll every 10s for new messages - ensure wsUnmatchedInboundMessageBufferSize(50)
                                         //is set high enough to handle the volume of messages in the poll window
