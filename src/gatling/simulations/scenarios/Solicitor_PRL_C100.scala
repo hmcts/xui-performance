62839734fb8f2eddb6992bcc1776908aca7e7895
pkryonas <paris.kryonas@solirius.com>
2026-05-11 15:07:36 +0100

    Removal of cookie-setting code which is no longer required and can actually break the journey.

--- src/gatling/simulations/scenarios/Solicitor_PRL_C100.scala
+++ src/gatling/simulations/scenarios/Solicitor_PRL_C100.scala
@@ -804,7 +804,7 @@ object Solicitor_PRL_C100 {
       //see xui-webapp cookie capture in the Homepage scenario for details of why this is being used.
       //after a period of time during a performance test, the cookie would change and subsequent calls would fail
       //with a 401 unauthorized, so this code is forcing the original cookie back in to the Gatling session
-      .exec(addCookie(Cookie("xui-webapp", "#{xuiWebAppCookie}").withMaxAge(28800).withSecure(false).withDomain(BaseURL.replace("https://",""))))
+      //May 2026 update: this cookie is no longer needed and actually can break the journey if included
 
       .exec(Common.configurationui)
       .exec(Common.configJson)
